34b3f147a1f509ac7bc86b8c8c90431368438702
Artem Kharchenko <gpsqaz1@gmail.com>
2025-03-05 22:28:55 +0700

    recursion trening

---
+++ Mentoring/OverloadingAndRecursion.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Practice
+{
+    public class OverloadingAndRecursion
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+        
+        private static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        
+        private static int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        
+        private static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+    
+    public class SquareAreaCalculator
+    {
+        public int CalculateArea(int side) //square
+        {
+            return side * side;
+        }
+
+        public int CalculateArea(int width, int height) //rectangle
+        {
+            return width * height;
+        }
+
+        public double CalculateArea(double radius) //circle
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+    
+    public class Recursion
+    {
+        public void OutputOfNumbers(int n)
+        {
+            if (n <= 0)
+                return;
+
+            Console.WriteLine(n);
+            OutputOfNumbers(n - 1);
+        }
+
+        public int OutputSumOfNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            
+            return n + OutputSumOfNumbers(n - 1);
+        }
+
+        public string ReverseString(string text)
+        {
+            if (text.Length <= 1)
+            {
+                return text;
+            }
+
+            return text[^1] + ReverseString(text[..^1]);
+        }
+
+        public int CountingLenghtOfString(string text)
+        {
+            if (text == "")
+            {
+                return 0;
+            }
+            
+            return 1 + CountingLenghtOfString(text.Substring(1));
+        }
+
+
+        public int SearchingSumOfNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            
+            return n + SearchingSumOfNumbers(n - 1);
+        }
+
+        public int SearchingFactorialOfNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1; 
+            }
+            
+            return n * SearchingFactorialOfNumbers(n - 1);
+        }
+        
+    }
+    
+}
--- Mentoring/obj/Debug/net9.0/Mentoring.AssemblyInfo.cs
+++ Mentoring/obj/Debug/net9.0/Mentoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mentoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693b09fa6abf619bda21e6d1d5741ef2ac2d7f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a5664aef442aa7144143b046cf0c353cb709c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mentoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mentoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
